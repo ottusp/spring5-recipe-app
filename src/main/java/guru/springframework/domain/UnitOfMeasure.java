61f0b53351b437b924bfe6b86905d3e29ec834a7
euseikodak <ottofernandes2012@gmail.com>
2020-07-27 14:58:10 -0300

    Created Ingredient and UnitOfMeasure entities and setted their relations with other classes.

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,16 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String uom;
+
+    @OneToOne
+    private Ingredient ingredient;
+}
